04bfdb4dec9ae8271de63d8ae9eae40d2592c9b4
Alvaro Rivoir <arivoir@gmail.com>
2018-06-29 20:11:22 -0300

    DigestAuthenticationFilter added

---
+++ DigestAuthentication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Open.Net.Http
+{
+    internal class DigestAuthentication
+    {
+        private static string _host;
+        private static string _user;
+        private static string _password;
+
+        public DigestAuthentication(string host, string user, string password)
+        {
+            _host = host;
+            _user = user;
+            _password = password;
+        }
+
+        private string CalculateMd5Hash(string input)
+        {
+            var inputBytes = ASCIIEncoding.ASCII.GetBytes(input);
+            var hashAlgorithm = MD5.Create();
+            var hash = hashAlgorithm.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+                sb.Append(b.ToString("x2"));
+            return sb.ToString();
+        }
+
+        public string GrabHeaderVar(string varName, string header)
+        {
+            var regHeader = new Regex(string.Format(@"{0}=""([^""]*)""", varName));
+            var matchHeader = regHeader.Match(header);
+            if (matchHeader.Success)
+                return matchHeader.Groups[1].Value;
+            throw new Exception(string.Format("Header {0} not found", varName));
+        }
+
+        //mode="GET"
+        public string GetDigestHeader(string mode, string dir, int nc, string cnonce, DateTime cnonceDate, string realm, string nonce, string qop)
+        {
+            nc = nc + 1;
+
+            var ha1 = CalculateMd5Hash(string.Format("{0}:{1}:{2}", _user, realm, _password));
+            var ha2 = CalculateMd5Hash(string.Format("{0}:{1}", mode, dir));
+            var digestResponse =
+                CalculateMd5Hash(string.Format("{0}:{1}:{2:00000000}:{3}:{4}:{5}", ha1, nonce, nc, cnonce, qop, ha2));
+
+            return string.Format("username=\"{0}\",realm=\"{1}\",nonce=\"{2}\",uri=\"{3}\"," +
+                "algorithm=\"MD5\",cnonce=\"{4}\",nc={5:00000000},qop=\"{6}\",response=\"{7}\"",
+                _user, realm, nonce, dir, cnonce, nc, qop, digestResponse);
+        }
+    }
+}
---
+++ DigestAuthenticationFilter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Open.Net.Http
+{
+    public class DigestAuthenticationFilter : RetryFilter
+    {
+        private DigestAuthentication _authentication;
+        private string _parameter;
+        private string _method;
+        private Uri _uri;
+        private int _nc = 0;
+        private NetworkCredential _credentials;
+
+        public DigestAuthenticationFilter(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override async Task<TimeSpan?> ShouldRetry(HttpResponseMessage response, int retries, CancellationToken cancellationToken)
+        {
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var wwwAuthenticate = response.Headers.WwwAuthenticate.FirstOrDefault();
+                if (wwwAuthenticate != null)
+                {
+                    var scheme = wwwAuthenticate.Scheme;
+                    if (scheme == "Digest")
+                    {
+                        _uri = response.RequestMessage.RequestUri;
+                        var server = _uri.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped);
+                        var innerHandler = InnerHandler as HttpClientHandler;
+                        if (innerHandler.Credentials != null)
+                        {
+                            _credentials = innerHandler.Credentials as NetworkCredential;
+                            InnerHandler = new HttpClientHandler();
+                        }
+                        var user = _credentials.UserName;
+                        var password = _credentials.Password;
+                        _authentication = new DigestAuthentication(server, user, password);
+                        _method = response.RequestMessage.Method.Method;
+                        _parameter = wwwAuthenticate.Parameter;
+                        return TimeSpan.Zero;
+                    }
+                }
+            }
+            return null;
+        }
+
+        protected override async Task<HttpRequestMessage> CopyRequestMessage(HttpRequestMessage request)
+        {
+            var message = await base.CopyRequestMessage(request);
+            var realm = _authentication.GrabHeaderVar("realm", _parameter);
+            var nonce = _authentication.GrabHeaderVar("nonce", _parameter);
+            var qop = _authentication.GrabHeaderVar("qop", _parameter);
+
+            var cnonce = Guid.NewGuid().ToString("N");
+            var cnonceDate = DateTime.Now;
+
+            message.Headers.Authorization = new AuthenticationHeaderValue("Digest", _authentication.GetDigestHeader(_method, _uri.AbsolutePath, _nc, cnonce, cnonceDate, realm, nonce, qop));
+            return message;
+        }
+    }
+}
